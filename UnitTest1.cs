c0a7d5e1d513a9b983c2e943d753ce127a142e05
alex.lin <alex.lin@nextfortune.tw>
2024-12-25 16:19:57 +0800

    init

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Budget_Team5;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
