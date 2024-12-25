c60fa3641f44404c65499d7c54773a05ecc4a0d7
joeychen <joeychen@odd-e.com>
2024-12-25 21:01:27 +0800

    extract method: DailyAmount()

--- BudgetService.cs
+++ BudgetService.cs
@@ -44,8 +44,7 @@ public class BudgetService
             if (targetBudget.YearMonth == start.ToString("yyyyMM"))
             {
                 var daysInStart = DateTime.DaysInMonth(start.Year, start.Month);
-                var dailyAmount = targetBudget.Amount / targetBudget.Days();
-                // var dailyAmount = targetBudget.Amount / daysInStart;
+                var dailyAmount = DailyAmount(targetBudget);
                 totalBudget += (decimal)(daysInStart - start.Day + 1) * dailyAmount;
             }
             else if (targetBudget.YearMonth == end.ToString("yyyyMM"))
@@ -60,5 +59,11 @@ public class BudgetService
         }
 
         return totalBudget;
+    }
+
+    private static int DailyAmount(Budget targetBudget)
+    {
+        var dailyAmount = targetBudget.Amount / targetBudget.Days();
+        return dailyAmount;
     }
 }
