d852e0931d2f0ab117402b9100557175c495fb21
joeychen <joeychen@odd-e.com>
2024-12-25 21:15:20 +0800

    remove empty if/else statement

--- Period.cs
+++ Period.cs
@@ -14,27 +14,12 @@ public class Period
 
     public int OverlappingDays(Budget budget)
     {
-        DateTime overlappingEnd = End < budget.LastDay()
+        var overlappingEnd = End < budget.LastDay()
             ? End
             : budget.LastDay();
-        DateTime overlappingStart = Start > budget.FirstDay()
+        var overlappingStart = Start > budget.FirstDay()
             ? Start
             : budget.FirstDay();
-        if (budget.YearMonth == Start.ToString("yyyyMM"))
-        {
-            // overlappingEnd = budget.LastDay();
-            // overlappingStart = Start;
-        }
-        else if (budget.YearMonth == End.ToString("yyyyMM"))
-        {
-            // overlappingEnd = End;
-            // overlappingStart = budget.FirstDay();
-        }
-        else
-        {
-            // overlappingEnd = budget.LastDay();
-            // overlappingStart = budget.FirstDay();
-        }
 
         return (overlappingEnd - overlappingStart).Days + 1;
     }
