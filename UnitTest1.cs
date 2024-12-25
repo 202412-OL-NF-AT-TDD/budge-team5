7f47321417779b76242bb72f56d71594267ae104
alex.lin <alex.lin@nextfortune.tw>
2024-12-25 18:06:51 +0800

    format

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -235,41 +235,39 @@ public class BudgetService(IBudgetRepo budgetRepo)
 
             return (decimal)budget.Amount / DateTime.DaysInMonth(start.Year, start.Month) * days;
         }
-        else
+
+        var monthKeys = new List<string>();
+
+        var startPointer = new DateTime(start.Year, start.Month, 1);
+        var endPointer = new DateTime(end.Year, end.Month, DateTime.DaysInMonth(end.Year, end.Month));
+
+        while (startPointer < endPointer)
         {
-            var monthKeys = new List<string>();
-
-            var a = new DateTime(start.Year, start.Month, 1);
-            var b = new DateTime(end.Year, end.Month, DateTime.DaysInMonth(end.Year, end.Month));
-
-            while (a < b)
-            {
-                monthKeys.Add(a.ToString("yyyyMM"));
-                a = a.AddMonths(1);
-            }
-
-            var targetBudgets = budgets.Where(bu => monthKeys.Contains(bu.YearMonth)).ToList();
-            var totalBudget = 0m;
-
-            foreach (var targetBudget in targetBudgets)
-            {
-                if (targetBudget.YearMonth == start.ToString("yyyyMM"))
-                {
-                    var daysInStart = DateTime.DaysInMonth(start.Year, start.Month);
-                    totalBudget += (decimal)(daysInStart - start.Day + 1) * targetBudget.Amount / daysInStart;
-                }
-                else if (targetBudget.YearMonth == end.ToString("yyyyMM"))
-                {
-                    var daysInEnd = DateTime.DaysInMonth(end.Year, end.Month);
-                    totalBudget += (decimal)end.Day * targetBudget.Amount / daysInEnd;
-                }
-                else
-                {
-                    totalBudget += targetBudget.Amount;
-                }
-            }
-
-            return totalBudget;
+            monthKeys.Add(startPointer.ToString("yyyyMM"));
+            startPointer = startPointer.AddMonths(1);
         }
+
+        var targetBudgets = budgets.Where(bu => monthKeys.Contains(bu.YearMonth)).ToList();
+        var totalBudget = 0m;
+
+        foreach (var targetBudget in targetBudgets)
+        {
+            if (targetBudget.YearMonth == start.ToString("yyyyMM"))
+            {
+                var daysInStart = DateTime.DaysInMonth(start.Year, start.Month);
+                totalBudget += (decimal)(daysInStart - start.Day + 1) * targetBudget.Amount / daysInStart;
+            }
+            else if (targetBudget.YearMonth == end.ToString("yyyyMM"))
+            {
+                var daysInEnd = DateTime.DaysInMonth(end.Year, end.Month);
+                totalBudget += (decimal)end.Day * targetBudget.Amount / daysInEnd;
+            }
+            else
+            {
+                totalBudget += targetBudget.Amount;
+            }
+        }
+
+        return totalBudget;
     }
 }
